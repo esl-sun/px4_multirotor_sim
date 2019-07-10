975f6364bee896c719870a3404d56b047bee98ff
Anton Erasmus <18177654@sun.ac.za>
2019-07-10 08:26:27 +0200

    Change HOME position to HRF airstrip

--- setup/init_sim_constants.m
+++ setup/init_sim_constants.m
@@ -6,6 +6,7 @@ g = 9.80665; % gravitational constant
 eta_w = 0.2; % eta constant
 rho = 1.225; % air density
 
-lat_runway = 47.397742; % HOME position latitude
-lon_runway = 8.545594; % HOME position longitude
-alt_runway = 488; % HOME position altitude
+% HOME - Helderberg Radio Flyers (HRF) Airstrip
+lat_runway = -34.046667; % HOME position latitude
+lon_runway = 18.740311; % HOME position longitude
+alt_runway = 8.0; % HOME position altitude
